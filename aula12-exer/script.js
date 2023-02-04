be9a3ad8681e0a110c45fe3c4463a5e7e28d30af
Joaquim Pinto <joaquimsp2000@gmail.com>
2023-02-04 15:17:31 -0300

    Update script.js

--- aula12-exer/script.js
+++ aula12-exer/script.js
@@ -6,11 +6,15 @@ function carregar() {
   var hora_atual = data.getHours()
 
   msg.innerHTML = `Agora são ${hora_atual} horas.`
+
   if(hora_atual >= 0 && hora_atual < 12) {
     img.src = '../assets/img/morning.png'
+    //document.body.style.background = '#ffdc44'
   } else if (hora_atual >= 12 && hora_atual <= 18) {
     img.src = '../assets/img/afternoon.png'
+    //document.body.style.background = '#b0e0fe'
   } else {
     img.src = '../assets/img/night.png'
+    //document.body.style.background = '#571e55'
   }
 }
