965e91eb69820335eb5f1e298b57474b9488e780
Joaquim Pinto <joaquimsp2000@gmail.com>
2023-02-08 18:56:09 -0300

    exercise about repetition

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// console.log('Tudo bem?')
+
+var version = 1
+
+while (version <= 10){
+    console.log(`Versão ${version}`)
+    version++
+}
+
+// do {
+//    console.log(`Versão ${version}`)
+//    version++
+// } while (version <= 10)
