cf54b4b150ae154a14505de9296c28668b1a1fe0
Joaquim Pinto <joaquimsp2000@gmail.com>
2023-02-11 11:35:30 -0300

    update

--- super-contador/script.js
+++ super-contador/script.js
@@ -1,5 +1,33 @@
 function countNum(){
+    var first = document.getElementById('num_first')
+    var last = document.getElementById('num_last')
+    var numb = document.getElementById('num')
     var msg = window.document.getElementById('message')
 
-    
+    if(first.value.length == 0 || last.value.length == 0 || numb.value.length == 0){
+        msg.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Preencha todos os campos!')
+    } else {
+        msg.innerHTML = `Contando: <br>`
+        var f = Number(first.value)
+        var l = Number(last.value)
+        var n = Number(numb.value)
+
+        if(n <= 0){
+            window.alert('Passo Inválido! Considerando PASSO 1')
+            n = 1
+        }
+        if(f < l){
+         // Contagem crescente   
+           for(count = f; count <= l; count += n){
+              msg.innerHTML += ` ${count} \u{1F449}`
+           }
+        } else {
+         // Contagem regressiva   
+           for(count = f; count >= l; count -= n){
+              msg.innerHTML += ` ${count} \u{1F449}`
+           }
+        }
+        msg.innerHTML += `\u{1F3C1}`
+    }
 }
