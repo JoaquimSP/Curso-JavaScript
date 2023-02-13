dfb78dbcda5add05663bdc72f0dc02d907f0bbe5
Joaquim Pinto <joaquimsp2000@gmail.com>
2023-02-13 19:23:54 -0300

    add new functions

--- analisador-de-numeros/script.js
+++ analisador-de-numeros/script.js
@@ -3,6 +3,21 @@ input_select = document.getElementById('txt_select')
 result = document.querySelector('div#result')
 valores = []
 
+function isNumber(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, list){
+    if(list.indexOf(Number(n)) != -1){
+        return true 
+    } else {
+        return false
+    }
+}
 
 function addNumber(){  
     var n = Number(num.value)    
